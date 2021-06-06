00c41762157e80ee7fcc02dbeb9be90a454823d3
YaseminBng <80950611+YaseminBng@users.noreply.github.com>
2021-06-06 17:01:06 +0200

    Abgabe

---
+++ 3.2/bspserver.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A3_2server = void 0;
+const Http = require("http");
+const Url = require("url");
+var A3_2server;
+(function (A3_2server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT); // Nimmt den aktuellen Port
+    if (!port)
+        port = 8100; // wenn es kein Port gibt, dann wird der Port mit dem wert 8100 initialisiert
+    let server = Http.createServer(); //erstellt neuen Server
+    server.addListener("request", handleRequest); // Server wird ein listener angehängt -> so wird  die Funktion handleRequest aufruft
+    server.addListener("listening", handleListen); // Server wird ein listener angehängt -> so wird   die Funktion handleListen aufruft
+    server.listen(port); //Server reagiert auf  definierten Port
+    function handleListen() {
+        console.log("listening");
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //zugangsberechtigung
+        let url = Url.parse(_request.url, true);
+        let query = url.query;
+        if (url.pathname == "/html") { //bin auf html?
+            for (let key in query) { // alle keys durchgehen
+                let value = query[key]; //verwende für jeden key den value
+                _response.write("<p>KEY: " + key + ", Value: " + value + "</p>"); //schreibe die Verbindung aus Key und Value
+            }
+        }
+        if (url.pathname == "/json") {
+            _response.write(JSON.stringify(query));
+        }
+        _response.end();
+    }
+})(A3_2server = exports.A3_2server || (exports.A3_2server = {}));
+//# sourceMappingURL=bspserver.js.map
---
+++ 3.2/bspserver.ts
@@ -0,0 +1,43 @@
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace A3_2server {
+    console.log("Starting server"); 
+    let port: number = Number(process.env.PORT); // Nimmt den aktuellen Port
+    if (!port)
+        port = 8100; // wenn es kein Port gibt, dann wird der Port mit dem wert 8100 initialisiert
+
+    let server: Http.Server = Http.createServer(); //erstellt neuen Server
+    server.addListener("request", handleRequest); // Server wird ein listener angehängt -> so wird  die Funktion handleRequest aufruft
+    server.addListener("listening", handleListen); // Server wird ein listener angehängt -> so wird   die Funktion handleListen aufruft
+    server.listen(port); //Server reagiert auf  definierten Port
+
+    function handleListen(): void {
+        console.log("listening"); 
+    }
+
+    interface Query {
+        [type: string]: string | string[];
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!"); 
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //zugangsberechtigung
+        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        let query: Query = url.query;
+
+        if (url.pathname == "/html") {       //bin auf html?
+            for (let key in query) {    // alle keys durchgehen
+                let value: string | string[] = query[key];  //verwende für jeden key den value
+                _response.write("<p>KEY: " + key + ", Value: " + value + "</p>"); //schreibe die Verbindung aus Key und Value
+            }
+        }
+        if (url.pathname == "/json") {      
+            _response.write(JSON.stringify(query));
+        }
+        _response.end();
+    }
+
+
+}
